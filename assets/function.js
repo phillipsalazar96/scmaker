feed7382052ef7bbb19168d2f7a591f18afad4f8
Phillip Salazar <phillipsalazar96@gmail.com>
2019-11-17 02:58:15 -0600

    UX for adding the shortcode

--- assets/function.js
+++ assets/function.js
@@ -1,5 +1,9 @@
 // es5
 const addformclass = "add_shortcode_boxes";
+
+allToHidden(addformclass);
+viewShortCode()
+
 function scmakerSelection()
 {
   var selector = document.getElementById('selecttype');
@@ -26,14 +30,58 @@ function scmakerSelection()
       changeViewing('video',addformclass)
       break;
 
-      case 'audio':
-      changeViewing('audio',addformclass)
-      break;
-
       default:
       allToHidden(addformclass);
     }
 
+    document.getElementById("display-area").innerHTML = "";
+    clearValue()
+}
+
+function viewShortCode()
+{
+  var selector = document.getElementById('selecttype');
+
+  var value = selector[selector.selectedIndex].value;
+
+  var listofid = ["text-box-form", "image-box-form", "link-box-form", "video-box-form", "audio-box-form"];
+  var cat = []
+  for (var i = 0; i < listofid.length; i++)
+  {
+    var idname = listofid[i].split("-");
+    cat.push(idname[0]);
+  }
+
+  for (var i = 0; i < cat.length; i++)
+  {
+    if (cat[i] === value)
+    {
+      var k = searchForValue(listofid[i]);
+      var lenwid = propImageSize("image");
+      console.log(k)
+      console.log(lenwid)
+      appendToHTML(value, k, lenwid)
+    }
+  }
+
+
+}
+
+function searchForValue(id)
+{
+  var arr = [];
+
+  var elemform = document.getElementById(id).childNodes;
+  var childofelem = elemform[1].childNodes;
+
+  for (var i = 0; i < childofelem.length; i++)
+  {
+    if (childofelem[i].value)
+    {
+      arr.push(childofelem[i].value);
+    }
+  }
+  return arr;
 
 }
 
@@ -49,6 +97,38 @@ function allToHidden(eleclass)
 
 }
 
+function appendToHTML(type, attr, misc)
+{
+  var displayarea = document.getElementById("display-area");
+
+
+  if (type == "text" )
+  {
+    displayarea.innerHTML = "<p>[" + attr[0] + "]</p><p>" + attr[1] + "</p>";
+  }
+  else if (type == "image" )
+  {
+
+    displayarea.innerHTML = "<p>[" + attr[0] + "]</p><img src='" + attr[1] + "' alt='" + attr[2] + "' height='" + misc[0] + "' width='" + misc[1] + "'" + "/>";
+  }
+  else if ( type == "link" )
+  {
+    displayarea.innerHTML = "<p>" + attr[0] + "</p><a href='" + attr[2] + "'>" + attr[1] + "</a>";
+  }
+  else if ( type == "video" )
+  {
+    //add video
+    var url = cutYoutubeUrl(attr[1])
+    displayarea.innerHTML = '<iframe width="1088" height="612" src="' + url + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+  }
+  else
+  {
+      displayarea.innerHTML = "";
+  }
+
+
+}
 
 function changeViewing(type,eleclass)
 {
@@ -67,4 +147,44 @@ function changeViewing(type,eleclass)
 
 }
 
-allToHidden(addformclass);
+function propImageSize(id)
+{
+  var arr;
+  if (document.getElementById(id + "sizemedium").checked)
+  {
+    arr = [400,400];
+  }
+  else if (document.getElementById(id + "sizelarge").checked)
+  {
+      arr = [600,600];
+  }
+  else
+  {
+      arr = [200,200];
+  }
+  return arr;
+}
+
+function cutYoutubeUrl(url)
+{
+  var str = "https://youtube.com/embed/" + url.substring(17);
+  return str;
+}
+// Go over it, I do not know how it works!
+function clearValue()
+{
+  var listofid = ["text-box-form", "image-box-form", "link-box-form", "video-box-form", "audio-box-form"];
+  for (var k = 0; k < listofid.length; k++)
+  {
+      var elemform = document.getElementById(listofid[k]).childNodes;
+      var childofelem = elemform[1].childNodes;
+      for (var i = 0; i < childofelem.length; i++)
+      {
+        var typed = childofelem[i].type
+        if (typed == "text")
+        {
+          childofelem[i].value = "";
+        }
+      }
+  }
+}
--- scmaker_add.php
+++ scmaker_add.php
@@ -31,60 +31,111 @@ else if (!empty($_POST['name_update']) && !empty($_POST['content_update']))
    <option value="audio">audio</option>
 </select>
 
+<button class="button button-primary" onclick="viewShortCode()">View!</button>
 
-<div id="t1" class="add_shortcode_boxes" name="text">
+<div id="text-box-form" class="add_shortcode_boxes" name="text">
 <form>
 <h4>Shortcode: Text</h4>
+<label>Name your shortcode</label>
+<br/>
 <input type="text" name="sc-name" placeholder="Shortcode name">
+<br/>
+<label>Add Text for yout shortcode</label>
+<br/>
 <input type="text" name="sc-content" placeholder="Content...">
+<input type="hidden" name="type" value="text"/>
 <?php submit_button("Add Shortcode") ?>
 </form>
 </div>
-<div id="t2" class="add_shortcode_boxes" name="image">
+
+
+
+
+<div id="image-box-form" class="add_shortcode_boxes" name="image">
   <form>
   <h4>Shortcode: Image</h4>
-  <input type="text" name="scname" placeholder="Shortcode name">
+  <label>Name your shortcode</label>
+  <br/>
+  <input type="text" name="scname" placeholder="Shortcode name" >
+  <br/>
+  <label>Add image source</label>
   <br/>
   <input type="text" name="sc-source" placeholder="Source...">
   <br/>
+  <label>Add an alternate text</label>
+  <br/>
   <input type="text" name="sc-alt" placeholder="Alternate text...">
   <br/>
-  <input type="number" name="sc-height">
+  <label>Hight of image</label>
   <br/>
-  <input type="number" name="sc-width">
+  <input id="imagesizesmall" type="radio" name="gender">Small<br>
+  <input id="imagesizemedium" type="radio" name="gender">Medium<br>
+  <input id="imagesizelarge" type="radio" name="gender">Large<br>
+
+  <input type="hidden" name="type" value="image"/>
   <?php submit_button("Add Shortcode") ?>
   </form>
 </div>
-<div id="t3" class="add_shortcode_boxes" name="link">
+
+
+
+
+<div id="link-box-form" class="add_shortcode_boxes" name="link">
   <form>
   <h4>Shortcode: Link</h4>
+  <label>Name your shortcode</label>
+  <br/>
   <input type="text" name="sc-name" placeholder="Shortcode name">
   <br/>
+  <label>Add your content for the link</label>
+  <br/>
   <input type="text" name="sc-content" placeholder="Content...">
+  <br/>
+  <label>Add url for link</label>
   <br/>
   <input type="text" name="sc-url" placeholder="Url...">
+  <input type="hidden" name="type" value="link"/>
   <?php submit_button("Add Shortcode") ?>
   </form>
 
 </div>
-<div id="t4" class="add_shortcode_boxes" name="video">
+
+<div id="view-box">
+</div>
+
+
+
+<div id="video-box-form" class="add_shortcode_boxes" name="video">
   <form enctype="multipart/form-data">
-  <h4>Shortcode: Text</h4>
+  <h4>Shortcode: Video</h4>
+  <label>Name your shortcode</label>
+  <br/>
   <input type="text" name="sc-name" placeholder="Shortcode name">
-  <input type="text" name="sc-content" placeholder="Content...">
-      <input type="file" name="fileToUpload" id="fileToUpload">
+  <br/>
+  <label>Add your video link</label>
+  </br>
+  <input type="text" name="sc-name" placeholder="Shortcode name">
+  <input type="hidden" name="type" value="video"/>
   <?php submit_button("Add Shortcode") ?>
   </form>
 </div>
 
-<div id="t5" class="add_shortcode_boxes" name="audio">
+
+
+<div id="audio-box-form" class="add_shortcode_boxes" name="audio">
   <form enctype="multipart/form-data">
-  <h4>Shortcode: Text</h4>
+  <h4>Shortcode: Audio</h4>
+  <label>Name your shortcode</label>
+  </br>
   <input type="text" name="sc-name" placeholder="Shortcode name">
+  </br>
+  <label>Add your audio link</label>
+  </br>
   <input type="text" name="sc-content" placeholder="Content...">
-  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="hidden" name="type" value="audio"/>
   <?php submit_button("Add Shortcode") ?>
   </form>
 </div>
 
+<div id="display-area">
 </div>
